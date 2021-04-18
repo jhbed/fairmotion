3cb86dc4f51c5fc7f4185abf27d5dd38451d9e99
Jake Bedard <jakehbedard@gmail.com>
2021-04-18 09:16:36 -0700

    changes original embeddings to use proper weights

--- fairmotion/models/spatio_temporal_transformer.py
+++ fairmotion/models/spatio_temporal_transformer.py
@@ -1,29 +1,39 @@
 from torch import nn
 from fairmotion.models.transformer import PositionalEncoding
 import torch
+from torch.autograd import Variable
+
+def convert_joints_from_3d_to_4d(tensor, N,M):
+    '''
+    input shape: (B, T, N*M) (ie. batch, seq_len, input_dim)
+    output shape: (B, T, N, M)
+    '''
+    return tensor.reshape(tensor.shape[0], tensor.shape[1], tensor.shape[2] // M, tensor.shape[2] // N)
+
+def convert_joints_from_4d_to_3d(tensor, N, M):
+    '''
+    input shape: (B, T, N, M) (ie. batch, seq_len, input_dim)
+    output shape: (B, T, N*M)
+    '''   
+    return tensor.reshape(tensor.shape[0], tensor.shape[1], tensor.shape[2] * tensor.shape[3])
 
 class SpatioTemporalTransformer(nn.Module):
 
-    def __init__(self, num_joints, embedding_size,  joint_vec_size=9, L=4, dropout_rate=0.1, num_heads=4, feedforward_size=256):
+    def __init__(self, N, D,  M=9, L=4, dropout_rate=0.1, num_heads=4, feedforward_size=256):
         """
-        :param num_joints: The number of joints that are in each pose in
+        :param N: The number of joints that are in each pose in
         the input.
-        :param embedding_size: The size of the initial joint embeddings.
+        :param D: The size of the initial joint embeddings.
         """
         super(SpatioTemporalTransformer, self).__init__()
 
-        self.embedding_layer = JointEmbeddingLayer(num_joints,
-                                                    embedding_size, 
-                                                    joint_vec_size)
-
-        self.position_encoding_layer = PositionalEncoding(num_joints*embedding_size, dropout_rate)
-
-        # TODO: Implement AttentionLayer
+        self.embedding_layer = JointEmbeddingLayer(N, D, M)
+        self.position_encoding_layer = PositionalEncoding(N*D, dropout_rate)
         self.attention_layers = nn.Sequential(
-                                    *[AttentionLayer(embedding_size*num_joints, num_heads, dropout_rate, feedforward_size) for _ in range(L)])
+                                    *[AttentionLayer(D*N, num_heads, dropout_rate, feedforward_size) for _ in range(L)])
 
         # one last linear layer (not sure what shapes to do yet)
-        self.final_linear_layer = nn.Linear(embedding_size*num_joints, joint_vec_size*num_joints)
+        self.final_linear_layer = nn.Linear(D*N, M*N)
                         
 
     def forward(self, inputs):
@@ -31,46 +41,41 @@ class SpatioTemporalTransformer(nn.Module):
         embeddings = self.position_encoding_layer(self.embedding_layer(inputs))
 
         #reverse batch and sequence length for attention layers because 
-        # nn.MultiheadAttention expects input of (T, B, H)
-        embeddings = embeddings.transpose(0,1)
+        # nn.MultiheadAttention expects input of (T, B, N*D)
+        embeddings = embeddings.permute(1,0, 2)
 
         out = self.attention_layers(embeddings)
         out = self.final_linear_layer(out)
         # Transpose back into (B, T, H)
-        out = out.permute(1,2,0)
+        out = out.permute(1,0,2)
+
         out += inputs # residual layer
         return out
 
 class JointEmbeddingLayer(nn.Module):
     
-    def __init__(self, num_joints, embedding_size, joint_vec_size=9):
-        """
-        Unlike the embedding layer from Assignment 4, the intitial embeddings 
-        are created as a simple linear layer.
-
-        One other difference in the paper is that it appears each joint should
-        have its own weights for creating the embedddings. To achieve this, the
-        linear layer is a weight matrix of the shape (N*M, N*H) with a bias 
-        vector of size (N*H)
-        """
+    def __init__(self, N, D, M=9):
+        """Transforms joint space M to embedding space D. Each joint has its own weights."""
         super(JointEmbeddingLayer, self).__init__()
-        self.linear = nn.Linear(in_features=num_joints*joint_vec_size, 
-                                out_features=num_joints*embedding_size)
+
+        # I do the W and bias initialization like this to ensure that the weights 
+        # are initialized exactly like Pytorch does it.
+        linears = [nn.Linear(in_features=M, out_features=D) for _ in range(N)]
+        self.W = Variable(torch.stack([lin.weight for lin in linears]).permute(0,2,1), requires_grad=True)
+        self.bias = Variable(torch.stack([lin.bias for lin in linears]).unsqueeze(0).unsqueeze(0), requires_grad=True)
+
+        # Saving these because they are helpful for reshaping inputs / outputs
+        self.M = M
+        self.N = N
 
     def forward(self, inputs):
         """
-        :param inputs: Joint input of shape (B, N*M, T) where N is the number of
-        joints, and M=9. 9 values are used to represent one joint. A joint is
-        represented as a rotation matrix, which in this case has been flattened
-        for our input.
-        :returns embeddings: floatTensor of shape (B, T, N*H), where H is the
-        embedding size. We can think of this as a 3d tensor of shape (T,N,H).
-        The reason we keep this as a 2d tensor is once again because joints do
-        not share weights across the transformer pipeline. If we represented
-        out input / output as 3d tensors, our weight layers would be applying 
-        the same weights to every joint.
+        input shape: (B, T, N*M) (ie. batch, seq_len, input_dim)
+        output shape: (B, T, N*D) 
         """
-        return self.linear(inputs.transpose(1,2))
+        inputs = convert_joints_from_3d_to_4d(inputs, self.N, self.M)
+        out = torch.einsum("btnm,nmd->btnd", inputs, self.W) + self.bias
+        return convert_joints_from_4d_to_3d(out, self.N, self.M)
 
 class AttentionLayer(nn.Module):
 
@@ -125,18 +130,25 @@ if __name__ == '__main__':
 
     print(torch.cuda.get_device_name(0))
     
-    N = 20
+    N = 24
     M = 9
+    D = 50
     T = 12
-    H = 20
-    B = 10
-    # N*M = 180
-    # H*N = 400
-    x = torch.rand(B, N*M, T)
+    B = 124
+    x = torch.rand(B, T, N*M)
 
-    model = SpatioTemporalTransformer(N, H)
+    model = SpatioTemporalTransformer(N, D)
+
     y = model(x)
     print(x.shape)
-    print(y.shape)
+    print(y.shape) # B, T, N*D
+    loss = y.sum()
+    loss.backward()
+
+    # picking a few modules randomely within the model to ensure 
+    # they have grad. ".grad" will give a warning or error if we did something 
+    # wrong.
+    print(model.attention_layers[0].linear1.weight.grad.shape)
+    print(model.embedding_layer.W.grad.shape)
     
 
